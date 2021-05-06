b9d01bf356c13fba6a1bde489e04a01e6ba87949
buddhhu <buddhhu@ultroid.tech>
2021-05-06 06:21:41 +0000

    pylint: auto fixes

--- plugins/saavn.py
+++ plugins/saavn.py
@@ -12,29 +12,55 @@
    `search song on saavn`
 """
 
-from . import *
-from telethon.tl.types import DocumentAttributeAudio
-import requests as r
-from urllib.request import urlretrieve
 import time
+from urllib.request import urlretrieve
 
-@ultroid_cmd(pattern='saavn ?(.*)')
+import requests as r
+from telethon.tl.types import DocumentAttributeAudio
+
+from . import *
+
+
+@ultroid_cmd(pattern="saavn ?(.*)")
 async def siesace(e):
     song = e.pattern_match.group(1)
     if not song:
-    	return await eod(e, '`Give me Something to Search')
+        return await eod(e, "`Give me Something to Search")
     hmm = time.time()
     lol = await eor(e, "`...`")
-    sung = song.replace(' ', '%20')
+    sung = song.replace(" ", "%20")
     url = f"https://jostapi.herokuapp.com/saavn?query={sung}"
     k = (r.get(url)).json()
     m = []
     for x in k:
-        m.append({'title': x['song'], 'url': x['media_url'], 'img': x['image'], 'duration': x['duration'], 'singers': x['singers']})
-    urlretrieve(m[0]['url'], m[0]['title']+".mp3")
-    urlretrieve(m[0]['img'], m[0]['title']+".jpg")
-    okk = await uploader(m[0]['title']+".mp3", m[0]['title']+".mp3", hmm, lol, "Uploading")
-    await ultroid_bot.send_file(e.chat_id, okk,attributes=[DocumentAttributeAudio(duration=int(m[0]['duration']), title=m[0]['title'], performer=m[0]['singers'])], supports_streaming=True, voice_note=True, thumb=m[0]['title']+".jpg")
+        m.append(
+            {
+                "title": x["song"],
+                "url": x["media_url"],
+                "img": x["image"],
+                "duration": x["duration"],
+                "singers": x["singers"],
+            }
+        )
+    urlretrieve(m[0]["url"], m[0]["title"] + ".mp3")
+    urlretrieve(m[0]["img"], m[0]["title"] + ".jpg")
+    okk = await uploader(
+        m[0]["title"] + ".mp3", m[0]["title"] + ".mp3", hmm, lol, "Uploading"
+    )
+    await ultroid_bot.send_file(
+        e.chat_id,
+        okk,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(m[0]["duration"]),
+                title=m[0]["title"],
+                performer=m[0]["singers"],
+            )
+        ],
+        supports_streaming=True,
+        voice_note=True,
+        thumb=m[0]["title"] + ".jpg",
+    )
     await lol.delete()
 
 
